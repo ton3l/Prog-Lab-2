58afe08979c8bf3a57f6695a9773812b69368533
Elton Nascimento <elton.felype.s.nascimento@gmail.com>
2026-09-22 15:59:14 -0300

    feat(lab2): start lab02 and start to work in intellij

---
+++ JogoDoBicho/src/JogoDoBicho.java
@@ -0,0 +1,35 @@
+import java.util.Map;
+
+public class JogoDoBicho {
+    public String[] bichos = {
+            "Avestruz",  // 01
+            "Águia",     // 02
+            "Burro",     // 03
+            "Borboleta", // 04
+            "Cachorro",  // 05
+            "Cabra",     // 06
+            "Carneiro",  // 07
+            "Camelo",    // 08
+            "Cobra",     // 09
+            "Coelho",    // 10
+            "Cavalo",    // 11
+            "Elefante",  // 12
+            "Galo",      // 13
+            "Gato",      // 14
+            "Jacaré",    // 15
+            "Leão",      // 16
+            "Macaco",    // 17
+            "Porco",     // 18
+            "Pavão",     // 19
+            "Peru",      // 20
+            "Touro",     // 21
+            "Tigre",     // 22
+            "Urso",      // 23
+            "Veado",     // 24
+            "Vaca"       // 25
+    };
+
+    public static void main(String[] args) {
+        JogoDoBicho jdb = new JogoDoBicho();
+    }
+}
