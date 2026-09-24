65133305f1a891737088dc9be8973324fae42d32
Elton Nascimento <elton.felype.s.nascimento@gmail.com>
2026-09-24 15:57:06 -0300

    feat: implement method getBicho and start to implement method apostar

--- JogoDoBicho/src/JogoDoBicho.java
+++ JogoDoBicho/src/JogoDoBicho.java
@@ -1,7 +1,9 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
 public class JogoDoBicho {
-    public String[] bichos = {
+    private String[] bichos = {
             "Avestruz",  // 01
             "Águia",     // 02
             "Burro",     // 03
@@ -29,7 +31,29 @@ public class JogoDoBicho {
             "Vaca"       // 25
     };
 
+    // Você falou que o override não é obrigatório, mas também falou que o override avisa ao compilador da sobrescrição, sem o override o compilador não identifica a sobrescrição? QUal a relação?
+
+    private List<int[]> apostas = new ArrayList<int[]>();
+
     public static void main(String[] args) {
         JogoDoBicho jdb = new JogoDoBicho();
+    }
+
+    public String getBicho(int id){
+        if (id > 25 || id < 1) throw new IllegalArgumentException("Número passado não corresponde a um bicho");
+        return this.bichos[id-1];
+    }
+
+    public boolean apostar(int[] aposta) {
+        if (aposta.length != 5) throw new IllegalArgumentException("A aposta deve ter 5 números");
+        for (int n : aposta){
+            if () throw new IllegalArgumentException("A aposta deve ter 5 números";
+        }
+        apostas.add(aposta);
+        return true;
+    }
+
+    public void getAposta(int id) {
+
     }
 }
